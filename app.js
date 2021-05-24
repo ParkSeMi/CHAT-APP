381df1e71439f6f3017ef7ebd5405d87f3c66248
ParkSeMi <qkrtpa12@naver.com>
2021-05-24 14:35:31 +0900

    no message

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
-const express = require("express");
+const express = require('express'); 
+const http = require('http'); 
+const app = express(); 
+const server = http.createServer(app); 
+const fs = require('fs'); 
+const io = require('socket.io')(server); 
 
-const app = express();
+app.use(express.static('src')); 
 
-const port = 8080;
+app.get('/', function (req, res) { 
+    fs.readFile('./src/index.html', (err, data) => { 
+        if (err) throw err; res.writeHead(200, { 
+            'Content-Type': 'text/html' 
+        })
+        .write(data)
+        .end(); 
+    }); 
+}); 
 
-app.set("port", port);
+io.sockets.on('connection', function (socket) { 
+    socket.on('newUserConnect', function (name) { 
+        socket.name = name; 
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+        io.sockets.emit('updateMessage', { 
+            name: 'SERVER', 
+            message: name + '님이 접속했습니다.' 
+        }); 
+    }); 
+    
+    socket.on('disconnect', function () { 
+        io.sockets.emit('updateMessage', { 
+            name: 'SERVER', 
+            message: socket.name + '님이 퇴장했습니다.' 
+        }); 
+    }); socket.on('sendMessage', function (data) {
+        data.name = socket.name; 
+        io.sockets.emit('updateMessage', data); 
+    }); 
+}); 
+
+server.listen(8080, function () { 
+    console.log('서버 실행중...'); 
 });
-
-app.listen(port, () => console.log("Listening on", port));
-
-module.exports = app;
